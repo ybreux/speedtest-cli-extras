30c611d653962d02efe994f7d5f4a44b1211a802
unknown <ybreux-ext@D-C4346B73554B.ad.in.ac-rennes.fr>
2016-02-12 15:27:57 +0100

    ajout des fichiers speedtest-tsv & speedtest-tsv.bat

---
+++ bin/speedtest-tsv.bat
@@ -0,0 +1,13 @@
+@echo off
+@rem ##################################################################
+@rem # Windows Batch script for launching speedtest-csv via
+@rem # bash, cf. MinGW [http://www.mingw.org/]
+@rem ##################################################################
+@rem # Temporarily add Batch script directory to the front of PATH,
+@rem # before calling bash script. This avoid having to have the
+@rem # bash script on the search PATH.
+set PATH_ORG=%PATH%
+set PATH_TMP=%~dp0;%PATH%
+rem set PATH=%PATH_TMP%
+bash --login -c "speedtest-tsv %*"
+set PATH=%PATH_ORG%
